02a740bc7b1f34e38e909a791d7d615255799dba
nastycrafz-web <nastycrafz@gmail.com>
2025-12-01 16:45:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -124,6 +124,5 @@ def set_webhook():
     bot.set_webhook(url=url)
     return "Webhook успешно установлен!", 200
 
-# ─────────────────────── ЗАПУСК ───────────────────────
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# ←←←←← ЭТО ДОЛЖНО БЫТЬ САМЫМИ ПОСЛЕДНИМИ СТРОКАМИ В ФАЙЛЕ
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
